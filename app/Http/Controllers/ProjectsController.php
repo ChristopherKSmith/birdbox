310750589081f52c7879fdb2b2814a8d52ac5206
kyle.smith <kyle.smith@steadysprint.com>
2021-08-12 21:32:15 -0400

    Added tests for invite users.

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -9,7 +9,7 @@ class ProjectsController extends Controller
     public function index()
     {
         return view('projects.index', [
-            'projects' => auth()->user()->projects,
+            'projects' => auth()->user()->availableProjects(),
         ]);
     }
 
