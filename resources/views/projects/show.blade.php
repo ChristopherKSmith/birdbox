dfde694256c51a662dfe39c88cf60051b1fd7499
kyle.smith <kyle.smith@steadysprint.com>
2021-08-14 22:34:38 -0400

    Updated html classes.

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,8 +4,9 @@
 
     <header class="flex items-center mb-3 py-4">
         <div class="flex justify-between items-end w-full">
-            <p class="text-default text-sm font-normal">
-                <a href="/projects">My Projects</a> / {{ $project->title }}
+            <p class="text-muted font-light">
+                <a href="/projects" class="text-muted no-underline hover:underline">My Projects</a> /
+                {{ $project->title }}
             </p>
 
             <div class="flex items-center">
@@ -29,7 +30,7 @@
                 {{-- tasks --}}
                 <div class="mb-8">
 
-                    <h2 class="text-lg text-default font-normal mb-3">Tasks</h2>
+                    <h2 class="text-lg text-muted font-light mb-3">Tasks</h2>
 
                     @foreach ($project->tasks as $task)
                         <div class="card mb-3">
@@ -37,8 +38,8 @@
                                 @method('PATCH')
                                 @csrf
                                 <div class="flex">
-                                    <input type="text" name="body" value="{{ $task->body }}"
-                                        class="bg-card text-default w-full {{ $task->completed ? 'text-default' : '' }}">
+                                    <input type="text" name="body" value="{{ $task->body }}" class="text-default
+                                            bg-card w-full {{ $task->completed ? 'line-through text-muted' : '' }}">
 
                                     <input type="checkbox" name="completed" onChange="this.form.submit()"
                                         {{ $task->completed ? 'checked' : '' }}>
@@ -57,7 +58,7 @@
 
                 {{-- general notes --}}
                 <div>
-                    <h2 class="text-lg text-default font-normal mb-3">General Notes</h2>
+                    <h2 class="text-lg text-muted font-light mb-3">General Notes</h2>
 
                     <form method="POST" action="{{ $project->path() }}">
                         @method('PATCH')
